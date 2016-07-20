593419955686337443f4286acc8f446a3b76960b
W <W@w.com>
2016-07-20 15:47:22 -0400

    wlau_add_3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace merge1
             Console.WriteLine("master_add_1");
             Console.WriteLine("wlau_add_1");
             Console.WriteLine("wlau_add_2");
+            Console.WriteLine("wlau_add_3");
         }
     }
 }
